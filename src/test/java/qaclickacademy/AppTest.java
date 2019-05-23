4433d756b38ddbd165f2696fa84e7b32e371f683
maneshap <manesha9@gmail.com>
2019-05-23 18:07:37 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+   
+    public void demo()
+    {
+       
+    	System.out.println("hey");  }
+
+   
+    
+}
